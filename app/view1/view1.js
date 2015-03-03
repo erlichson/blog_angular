8bacf641787fb9b1ed30d218b124ca8eb86f2251
Andrew Erlichson <aje@desktop.erlichson.net>
2015-03-02 21:00:15 -0500

    minor changes

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -9,11 +9,16 @@ angular.module('myApp.view1', ['ngRoute'])
   });
 }])
 
-.controller('View1Ctrl', ['$scope', '$http', function($scope, $http) {
+.controller('View1Ctrl', ['$scope', '$http', '$sce', function($scope, $http, $sce) {
     
 
     $http.get('http://localhost:8080/recent_posts').success(function(data) {
-	$scope.posts = data
+	$scope.posts = data;
+	for (var i = 0; i < data.length; i++) {
+	    data[i].num_comments = data[i].comments.length;
+	    data[i].body = $sce.trustAsHtml(data[i].body);
+	}
+
     }).
     error(function(data) {
 	$scope.posts = [{'title':'Error loading posts'}]
